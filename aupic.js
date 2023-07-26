fbc57b3c6e9ebd10d22deef809f82499129a747d
au-pic <137679878+au-pic@users.noreply.github.com>
2023-07-26 22:02:48 +0530

    updated

--- aupic.js
+++ aupic.js
@@ -1,8 +1,29 @@
+function openNav() {
+  document.getElementById("myTopnav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("myTopnav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+
+
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("myTopnav");
+
+// Get the offset position of the navbar
+var sticky = myTopnav.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+  if (window.pageYOffset >= sticky) {
+    myTopnav.classList.add("sticky")
   } else {
-    x.className = "topnav";
+    myTopnav.classList.remove("sticky");
   }
 }
